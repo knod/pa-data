81a7c33c6464fbf3cb37a82fd2667c8204b4ed82
knod <3197142-knod@users.noreply.gitlab.com>
2019-06-29 15:42:50 -0400

    Faster mdj test

--- mdj-names3-test.js
+++ mdj-names3-test.js
@@ -69,7 +69,6 @@ let type = 'mdj';
 
 // Standard
 let names = JSON.parse(fs.readFileSync('names3.json', 'utf8'));
-// let names = require('./names3.json');
 const dates = {start: "06/01/2018", end: "12/31/2018"};
 let throttle = 15 * 1000;
 let timesRepeated = 0;
@@ -78,31 +77,90 @@ let timesRepeated = 0;
 // latest: node cp-names.js 41 45
 let namesStartIndex = parseInt(process.argv[2]),
     namesEndIndex   = parseInt(process.argv[3]);
+let doPlaySound = process.argv[5];
 
 if (process.argv[4]) {
   throttle = parseInt(process.argv[4]);
 }
-fs.writeFileSync(nameIndexPath, namesStartIndex);
 
+// // command line command looks something like:
+// // node mdj-names3-test.js mdj '{"wait":"200","getFrom":"names3.js"}'
+// let type = process.argv[2];
+// let commandLineArgs = JSON.parse(process.argv[3]);
+
+// if (!commandLineArgs.user) {
+//   throw ReferenceError('You must at least write \'{"user":"yourFirstNameHere"}\' after the script name to add the "user" property.'.yellow);
+// }
+// if (commandLineArgs.user === 'yourFirstNameHere') {
+//   throw Error('Replace "yourFirstNameHere" with your actual first name. Sorry for not being clear.'.yellow)
+// }
+// if (commandLineArgs.startIndex && !commandLineArgs.endIndex) {
+//   commandLineArgs.endIndex = commandLineArgs.startIndex + 1000;  // names.length - 1
+// }
+
+// // In future 'type' will determine what selector and filename values are used
+// // let nameIndexPath = type + '-name-index.json';
+// let nameIndexVal = require('./' + nameIndexPath);
+// console.log('nameIndex required:', nameIndexVal);
+// let defaultArgs = {
+//   startIndex: nameIndexVal,
+//   endIndex: nameIndexVal + 1000,
+//   startYear: 2017,
+//   endYear: 2019,
+//   wait: 300,
+// //   volume: 10,  // no way to implement this right now
+//   alerts: 'yes',
+//   getFrom: 'names3.json',
+// //   type: type,
+// }
+
+// let argvs = Object.assign(defaultArgvs, commandLineArgvs);
+// argvs.dates = {
+//   start: '01/01/' + argvs.startYear,
+//   end: '12/31' + argvs.endYear,
+// }
+// if (argvs.endYear === 2019) { argvs.dates.end = '06/25/2019'; }
+
+// // // In future 'type' will determine what selector and filename values are used
+// // let vars = whatever[type];
+// // // In future, which file to get names from will be determined by
+// // // files named after users
+// // if (!args.user || args.user === 'yourNameHere') {
+// //   throw ReferenceError('You must at least write \'{"user":"yourFirstNameHere"}\' after the script name to add the "user" property.'.yellow);
+// // }
+// // if (args.user === 'yourFirstNameHere') {
+// //   throw Error('Replace "yourFirstNameHere" with your actual first name. Sorry for not being clear.'.yellow)
+// // }
+// // if (commandLineArgs.startIndex && !commandLineArgs.endIndex) {
+// //   argvs.endIndex = names.length - 1
+// // }
+// // console.log(args);
+
+
+/*
+let namesStartIndex = argvs.startIndex;
+let namesEndIndex = argvs.endIndex;
+let doPlaySound = argvs.alerts;
+let throttle = argvs.wait;
+*/
+
+fs.writeFileSync(nameIndexPath, namesStartIndex);
 console.log(namesStartIndex, namesEndIndex);
+
+
 
 async function byNamesDuring (dates, browser) {
 
   let err = null;
-  // fs.writeFileSync(nameIndexPath, namesStartIndex);
-
-  // // Get last stored name index (number)
-  // let nameIndex = JSON.parse(fs.readFileSync(nameIndexPath, 'utf8'));
-
-  // // Get next name after that
-  // nameIndex += 1;
-  // let names = JSON.parse(fs.readFileSync('names3.json', 'utf8'));
-  // let name = names[nameIndex];
-  // console.log(name);
 
   // submit to site along with date
   const page = await browser.newPage();
   await page.setViewport({width: 1920, height: 2000});
+  page.on('console', consoleMessageObject => function (consoleMessageObject) {
+    if (consoleMessageObject._type !== 'warning') {
+      console.debug(consoleMessageObject._text)
+    }
+  });
 
   await page.goto(url)
 
@@ -130,7 +188,6 @@ async function byNamesDuring (dates, browser) {
 
   // Get last stored name index (number)
   let nameIndex = JSON.parse(fs.readFileSync(nameIndexPath, 'utf8'));
-  // let names = JSON.parse(fs.readFileSync('names3.json', 'utf8'));
 
   while (nameIndex <= namesEndIndex) {
     console.log('~\n~\n~\n~\n~\nName index: ' + nameIndex + '\n~\n~\n~\n~\n~\n');
@@ -241,12 +298,28 @@ async function byNamesDuring (dates, browser) {
     if (found === false) {return ['not found', err];}
     page.click(searchSelector)
 
-    let pageData = {done: false, page: 0};
+    // Search through the pages
+    let pageData = {done: false, page: 0, err: {message: null, value: null}};
     while (!pageData.done) {
+
+      // Make sure page is still there
+      await page.waitForSelector(searchSelector)
+        .catch(function(theError){
+          err = theError;
+          found = false;
+          console.log('elment not found. 7.5')
+        });
+      if (found === false) {return ['not found', err];}
+
       console.log(1.5);
       pageData = await getPDFs(browser, page, pageData.page);
       console.log(17);
       console.log('pageData', pageData);
+
+      // Return from error if needed
+      if (pageData.err && (pageData.err.message || pageData.err.value)) {
+        return [pageData.err.message, pageData.err.value];
+      }
     }  // ends while this name not done
 
     console.log(18)
@@ -272,26 +345,63 @@ async function getPDFs (browser, page, lastPageNum) {
   let newPageNum = lastPageNum + 1;
   console.log('new pg', newPageNum)
 
-  // await page.waitForSelector(
-  //     '#loading[style*="display: none;"]',
-  //     {timeout: 180000}
-  // );
+  let startTime = Date.now()
+  console.log('start looking for result:', Date().toString());
 
-  let noResults = false;
-  await page.waitForSelector(
-      resultsSelector,
-      {timeout: 40000}
-  ).catch(function(err){
-    // if no results, skip this page?
-    noResults = true;
-    console.log('no results');
-    return 'no results';
-    // console.log(err);
-  });
-  // If no results, return to continue loop
-  if (noResults) {
+  let anError = null;
+  let resultsElemFound = page.waitForSelector(resultsSelector);
+  let noResultsElemFound = page.waitForSelector(noResultsSelector);
+  let foundSomeResults = false;
+  let foundNoResults = false;
+  await Promise.race([resultsElemFound, noResultsElemFound])
+    .then(function(value) {
+      console.log('race value:', value._remoteObject.description);
+      foundSomeResults = value._remoteObject.description.indexOf(resultsElemFound) >= 0
+      foundNoResults = !foundSomeResults
+      console.log('results found?', foundSomeResults);
+      console.log('no results found?', foundNoResults);
+      // Both resolve, but promise2 is faster
+    })
+    .catch(function (theError) {
+      anError = theError;
+    });
+
+  let endTime = Date.now();
+  let elapsed = endTime - startTime;
+  console.log('Time elapsed to find results:', elapsed, '(seconds:', elapsed/1000 + ')');
+
+  if (foundNoResults) {
     return {done: true, page: null};
   }
+  if (!foundNoResults && !foundSomeResults) {
+    return {done: true, page: null, err: {message: 'not found', value: anError}}
+  }
+
+  // if (type === 'cp' && !foundResults) {
+  //   return {done: true, page: null}; 
+  // }
+
+  // if (!foundResults) {
+  //   try {
+  //     await page.waitFor(
+  //       function (noResultsSelector, noResultsText) {
+  //         let elem = document.querySelector(noResultsSelector);
+  //         let text = elem.innerText;
+  //         let hasNoResults = text === noResultsText;
+  //         return isNew;
+  //       },
+  //       {},
+  //       noResultsSelector, noResultsText
+  //     )
+  //   } catch (theError) {
+  //     // Didn't find any expected elements
+  //     return {
+  //       value: 'not found',
+  //       err: theError,
+  //     };
+  //   }
+  // }
+////
 
   // If there were results, we can start the repeat count again.
   timesRepeated = 0;
@@ -356,6 +466,11 @@ async function getPDFs (browser, page, lastPageNum) {
     // console.log(err);
   });
   console.log('paginated:', paginated, ', nav:', navText);
+  if (navText) {
+    let pages = navText.match(/\d+/g);
+    let lastPage = pages[pages.length - 1];
+    console.log('current last page:', lastPage);
+  }
 
   console.log(5);
   // go down rows getting links and ids
@@ -410,7 +525,7 @@ async function getPDFs (browser, page, lastPageNum) {
     // We just want CP data, or so they tell us
     if (requiredPrefix.test(id)) {
       let text = '\n' + Date.now() + '_' + id + '_page_' + newPageNum;
-      let fixedText = text + '_stabilized_06-18-12-18';
+      let fixedText = text + '_stabilized_06_18_12_18';
       // fixed at cp-names3 20184
 
       // save docket id to dockets-used.txt?
@@ -515,7 +630,6 @@ async function downloadPDF(pdfURL, outputFilename) {
 
 // Test
 
-let doPlaySound = process.argv[5];
 async function startNewBrowser () {
 
   let browser = await puppeteer.launch({ headless: true });
@@ -532,8 +646,8 @@ async function startNewBrowser () {
           console.log('\n#\n#\n# >> Let this go till log says "giving up". Or stop it yourself and deal with it a different way. 1\n#\n#\n#');
           console.log(err.statusCode)
           if (err.statusCode === 429) {
-            console.log('waiting one minute');
-            setTimeout(waitThenRepeat, 60000);
+            console.log('waiting two minutes');
+            setTimeout(waitThenRepeat, 120000);
           } else {
             // repeat with increased wait
             waitThenRepeat();
@@ -569,8 +683,8 @@ async function startNewBrowser () {
       console.log('\n#\n#\n#\n### Let this go till log says "giving up". Or stop it yourself and deal with it a different way. 2\n#\n#\n#');
       console.log(err.statusCode)
       if (err.statusCode === 429) {
-        console.log('waiting one minute');
-        setTimeout(waitThenRepeat, 60000);
+        console.log('waiting two minutes');
+        setTimeout(waitThenRepeat, 120000);
       } else {
         waitThenRepeat();
       }
